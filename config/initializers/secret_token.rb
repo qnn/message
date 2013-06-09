dfa250e5359ca0c90b46620f1659adaec07fea0b
caiguanhao <caiguanhao@gmail.com>
2013-06-09 17:19:40 +0800

    initial commit: rails new;

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contactus::Application.config.secret_token = '2e2d750e74fcf705b8e4ecfcc3dee87ada0f432147d2d85fbf26c17b6249536f59cca517a0e90a8f8561bb8d7ac462e301fdb2650c749fe91e2cfe07763a87df'
