650ee05db24f49d1e9e531caac49584b71bb8991
caiguanhao <caiguanhao@gmail.com>
2013-06-09 20:34:53 +0800

    reverse order; some translations;

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
+    @messages = Message.all(:order => "created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
@@ -44,7 +44,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
+        format.html { redirect_to @message, notice: t("messages.created") }
         format.json { render json: @message, status: :created, location: @message }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.update_attributes(params[:message])
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
+        format.html { redirect_to @message, notice: t("messages.updated") }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
