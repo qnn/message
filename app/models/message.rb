96baffc37c260e33d184affbb821c9284d15427b
caiguanhao <caiguanhao@gmail.com>
2013-06-09 21:05:58 +0800

    restrict and validates message fields length;

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
   attr_accessible :content, :name, :title
   validates_presence_of :name, :title, :content
+  validates :name, :length => { :maximum => 60 }
+  validates :title, :length => { :maximum => 200 }
+  validates :content, :length => { :maximum => 20000 }
 end
