2adfead84be6540464dbd7d03ec3fb647d1f8370
caiguanhao <caiguanhao@gmail.com>
2013-06-09 18:33:56 +0800

    add messages: rails g scaffold message name:string title:string content:text;

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Contactus::Application.routes.draw do
+  resources :messages
+  root :to => "messages#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
