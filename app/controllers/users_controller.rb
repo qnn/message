6b03bc143fc8677924e0982772abc7548feed992
caiguanhao <caiguanhao@gmail.com>
2013-06-15 16:07:41 +0800

    add create new user;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
 
   before_filter :list_of_roles, :only => [:edit, :update]
+  before_filter :list_of_roles2, :only => [:new, :create]
+
+  def list_of_roles2
+    if User::ROLES.include? current_user.role
+      index = User::ROLES.index(current_user.role)
+      if index == 0
+        @roles = User::ROLES.drop(1)
+      else
+        @roles = User::ROLES.drop(index)
+      end
+    else
+      @roles = User::ROLES.drop(1)
+    end
+  end
 
   def index
-    @users = User.all(:order => "created_at DESC")
+    @users = User.all(:order => "created_at ASC")
 
     respond_to do |format|
       format.html
@@ -16,6 +30,28 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html
       format.json { render json: @user }
+    end
+  end
+
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+
+  def create
+    @user = User.new(params[:user])
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to new_user_url, notice: t("users.created") }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
